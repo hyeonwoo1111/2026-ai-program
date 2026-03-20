c59b6728e02d034022d94c60dcf7aa43b9a1831d
hyeonwoo1111 <abg195@naver.com>
2026-03-20 10:09:18 +0900

    Add files via upload

---
+++ ai 2주차 과제.py
@@ -0,0 +1,67 @@
+print('나의 이름은 :' '홍길동')
+print('나의 나이는 :', 27)
+print('나의 키는', 179, 'cm 입니다.')
+print('10 20=', 10 + 20)
+print('10 * 20 =', 10 * 20)
+name = '전우치'
+print('나의 이름은 :', name)
+age= 27
+print('나의 나이는 :', age)
+height = 179
+print('나의 키는', height, 'cm 입니다.')
+sum =10 + 20
+print('10 + 20=', sum)
+mult =10 * 20
+print('10 * 20 =', mult)
+width = 20
+height = 40
+width = 30
+area = width * height
+print('사각형의 면적', area)
+print(123 * 456)
+print(1357 + 2468)
+print(5 ** 4)
+print(10 / 4)
+print(10 // 5)
+print(10%5)
+
+print(5%2)
+
+
+print(2**0.5)
+print(3**0.5)
+a = 8 + 2j
+b = 4 + 3j
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+a=1024
+print(a>>1)
+
+print(a>>2)
+
+a=a>>1
+print(a)
+a=a>>1
+print(a)
+a=1
+print(a<<1)
+a=a<<1
+print(a)
+a=a<<1
+print(a)
+a=a<<1
+print(a)
+a=a<<1
+print(a)
+name = input('이름을 입력하세요;')
+print(name, '님이 입장하셨습니다.')
+m=int(input('숫자 m을 입력하세요: '))
+n = int(input('숫자 n을 입력하세요: '))
+print('m + n =', m +n)
+print('m - n =', m -n)
+
+radius = int(input('반지름 radius를 입력하세요: '))
+print('원의 면적 =', radius*radius*3.14)
