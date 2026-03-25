02e92ba27d65e8ac3ebc7483864d533dc7636fe7
hyeonwoo1111 <abg195@naver.com>
2026-03-25 16:47:52 +0900

    1234

--- 4weeks.py
+++ 4weeks.py
@@ -1,3 +1,9 @@
-for i in range(5):
-    print("Welcom to everone")
-    
+'''
+score = [87,84,95,67,88,94,63]
+names = ['영호','순자','영식','순희',score]
+adbook = ['영호','25','010-1234-0000',],['철수','23','010-1111-1111',]
+for i in adbook:
+    print(i)
+'''
+ri = list(range(5))
+print(ri)
